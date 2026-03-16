947eaea1e3bdf4ed3cb84c0ae215b03a6bdf3a26
Aaraan Mahmood <151668629+aairhann17@users.noreply.github.com>
2026-03-16 14:08:48 -0400

    Remove configure method from Application class
    Removed the overridden configure method from Application class.

--- BinaryCalculatorWebApp/src/main/java/com/ontariotechu/sofe3980u/Application.java
+++ BinaryCalculatorWebApp/src/main/java/com/ontariotechu/sofe3980u/Application.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
